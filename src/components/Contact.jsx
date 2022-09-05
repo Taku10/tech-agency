cd7f90a144eb4c558999df91d4dbae3813eefba3
Taku10 <tvmadziwa5@gmail.com>
2022-09-05 22:19:23 +0200

    init

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,33 @@
 import '../style/contact.css'
-import React from 'react'
+import React, {useState} from 'react'
 
 const Contact = () => {
+    const[input, setInput]=useState({
+        name: '',
+        email:'',
+        message:'',
+    })
+
+    const handleChange =(event)=>{
+        const{name, value}= event.target;
+        setInput((prev)=>{
+            return{
+                ...prev,
+                [name]: value,
+            }
+        })
+    }
+
+    const{name, email, message}= input;
+
+    const handleSumbit =(event)=>{
+        event.preventDefault();
+        console.log(message)
+        setInput('')
+    }
+
+
+
   return (
     <div className='contact-container'>
       <div className='contact-heading'>
@@ -9,18 +35,18 @@ const Contact = () => {
         <p>We appreciate your ideas</p>
       </div>
       <div className='contact-form'>
-        <form >
+        <form onSubmit={handleSumbit}>
             <div className='name '>
                 <label htmlFor="name">NAME</label>
-                <input type="text" name="name" id="name" value='' placeholder='Enter your name'/>
+                <input type="text" name="name" id="name" value={name} placeholder='Enter your name' onChange={handleChange} />
             </div>
             <div className='email'>
                 <label htmlFor="email">EMAIL ADDRESS</label>
-                <input type="text" name="email" id="email" value='' placeholder='Enter your name' />
+                <input type="text" name="email" id="email" value={email} placeholder='Enter your name' onChange={handleChange} />
             </div>
             <div className='message'>
                 <label htmlFor="message">YOUR MESSAGE</label>
-                <textarea name="message" id="message" cols="30" rows="1" placeholder='Enter Message'></textarea>
+                <textarea name="message" id="message" value={message} cols="30" rows="1" placeholder='Enter Message' onChange={handleChange} ></textarea>
             </div>
             <div className='submit-button'>
                 <button>Submit</button>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,18 +3,43 @@ import '../style/footer.css'
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div className='footer-container'>
-        <nav className='footer-nav'>
-            <h1>Taku Agency</h1>
-            <ul className="menu-items">
-                <div className='footer-header'>
-                    Menu
+    return (
+        <div className='footer-container'>
+            <div className='footer-logo'>
+                <h1>TAKU AGENCY</h1>
+            </div>
+
+            <div className='details'>
+                <div className='footer-item menu'>
+                    <h5 className='footer-heading'>MENU</h5>
+                    <ul className='menu-list'>
+                        <li><a href="">ABOUT</a></li>
+                        <li><a href="">SERVICES</a></li>
+                        <li><a href="">PORTFOLIO</a></li>
+                        <li><a href="">CONTACT</a></li>
+                    </ul>
                 </div>
-            </ul>
-        </nav>
-    </div>
-  )
+                <div className='footer-item address'>
+                <h5 className='footer-heading'>ADDRESS</h5>
+                    <ul className='address-list'>
+                        <li>344</li>
+                        <li>Corporation Building</li>
+                        <li>Verkeer Street</li>
+                        <li>Cape Town</li>
+                        <li>7994</li>
+                    </ul>
+                </div>
+                <div className="footer-item contact">
+                <h5 className='footer-heading'>CONTACT</h5>
+                    <ul className='contact-list'>
+                        <li>E:info@takuagency.com</li>
+                        <li>T:+27 23 564 5566</li>
+                        <li>F:+27 234 346 8976</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Footer
